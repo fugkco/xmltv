c881934292abd7eba7e66901c6e29a0ae94d75ad
fugkco <fugkco@users.noreply.github.com>
2021-06-25 19:21:04 +0100

    Some final fixes, I hope

--- scripts/get-guide-channels.sh
+++ scripts/get-guide-channels.sh
@@ -11,16 +11,19 @@ block_list_file=""
 allow_list_file="$dir/guide-allow-list.txt"
 
 get_guides() {
-  curl --fail --silent --show-error --location \
-    --header 'Content-Type: application/x-www-form-urlencoded' \
-    --data-raw 'thisDay=&thisTime=&gridSpan=&emailaddress=&regionid=9&systemid=5&xn=Show+me+the+channels' \
-    'https://www.tvguide.co.uk/mychannels.asp'
+  for systemid in 3 5 25; do
+    curl --fail --silent --show-error --location \
+      --header 'Content-Type: application/x-www-form-urlencoded' \
+      --data-raw "thisDay=&thisTime=&gridSpan=&emailaddress=&regionid=9&systemid=${systemid}&xn=Show+me+the+channels" \
+      'https://www.tvguide.co.uk/mychannels.asp'
+  done
 }
 
 get_all_channels() {
   get_guides |
     grep -Eo "(add|delete)Channel\(([0-9]+),\s*'([^']+)'\)" |
     sed -r "s/^(add|delete)Channel\(([0-9]+),\s*'([^']+)'\)/\2,\3/" |
+    sort -u |
     sort -t, -k2
 }
 
@@ -30,25 +33,29 @@ get_blocked_channels() {
 
 get_channel_list() {
   if [[ -n "$allow_list_file" ]] && [[ -r "$allow_list_file" ]]; then
-    sed -r -e 's/([^[:alnum:]])/\\\1/g' -e 's/^/,/' -e 's/$/\$/' "$allow_list_file" | grep -f - <(get_all_channels)
+    sed -r -e 's/([^[:alnum:]])/\\\1/g' -e 's/^/,/' -e 's/$/\$/' "$allow_list_file" | grep -E -f - <(get_all_channels)
   elif [[ -n "$block_list_file" ]] && [[ -r "$block_list_file" ]]; then
-    sed -r -e 's/([^[:alnum:]])/\\\1/g' -e 's/^/,/' -e 's/$/\$/' "$block_list_file" | grep -v -f - <(get_all_channels)
+    sed -r -e 's/([^[:alnum:]])/\\\1/g' -e 's/^/,/' -e 's/$/\$/' "$block_list_file" | grep -E -v -f - <(get_all_channels)
   else
     get_all_channels
   fi
 }
-
+1012
 get_allowed_channels() {
   local id channel
 
   while read -r channel; do
     id="$(awk -F, '{print $1}' <<<"$channel" | xargs)"
+    name="$(awk -F, '{print $2}' <<<"$channel" | xargs)"
     if ! curl --fail --silent --show-error --location --no-buffer "https://www.tvguide.co.uk/channellistings.asp?ch=${id}&cTime=$(date +'%-m/%-d/%Y%%20%-I:00:00%%20%p')&thisTime=&thisDay=" 2>/dev/null | grep -qF '/HighlightImages/'; then
+      >&2 printf "channel %s (name=%s) does not seem to have valid pages. Skipping..\n" "$id" "$name"
       continue
     fi
 
+    >&2 printf "channel %s (name=%s) is valid!\n" "$id" "$name"
+
     echo "$channel"
-  done < <(get_channel_list)
+  done < <(get_channel_list | grep -vE ',$')
 }
 
 get_channel_id() {
